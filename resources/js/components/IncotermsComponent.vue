a2f4167127693c0a01fa15833696946d99aa664a
raulh <raulhernandezg2001@gmail.com>
2026-05-11 01:12:33 +0200

    s

--- resources/js/components/IncotermsComponent.vue
+++ resources/js/components/IncotermsComponent.vue
@@ -25,7 +25,7 @@
         <!-- Main modal -->
         <div id="insert-modal" data-modal-backdrop="static" data-modal-placement="top-center" tabindex="-1"
             aria-hidden="true"
-            class="bg-white border border-gray-200 rounded-xl shadow-md p-5 w-72 hover:shadow-lg transition duration-300">
+            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
             <div class="relative p-4 w-full max-w-md max-h-full" :style="{ backgroundColor: white }">
                 <!-- Modal content -->
                 <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
