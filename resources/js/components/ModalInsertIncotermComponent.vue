b1bac6aa56b80945c810e4625b59eecbe923c4ea
unknown <raulhernandezgarcia2001@gmail.com>
2026-05-11 17:34:29 +0200

    control de errores anyadido en modal insert incoterm

--- resources/js/components/ModalInsertIncotermComponent.vue
+++ resources/js/components/ModalInsertIncotermComponent.vue
@@ -33,7 +33,7 @@
                             <input type="text" name="codi"
                                 class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50
                         border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autofocus v-model="incoterm.codi" />
+                                placeholder=" " autofocus maxlength="5" v-model="incoterm.codi" />
                             <label class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10
                         origin-left start-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100
                         peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">
@@ -95,7 +95,21 @@ const esError = ref(false)
 const mensajeError = ref();
 
 const insertIncoterms = () => {
-    const targetEl = document.getElementById("#insert-modal");
+
+    /*Control de errores: Si el valor del campo código supera más de 5 carácteres
+    no dejará guardar los datos en la app (incluida en la BD)*/
+
+    //Reset del error (cada vez que haga un insert en la BD)
+    esError.value = false;
+    mensajeError.value = '';
+
+    //Código que realiza dicho control de errores
+    if (!incoterm.value.codi || incoterm.value.codi.length > 5) {
+        esError.value = true;
+        mensajeError.value = 'La longitud de carácteres del código no puede superar más de 5';
+    }
+
+    const targetEl = document.getElementById("insert-modal");
     const modal = new Modal(targetEl);
 
     estaCargando.value = true;
