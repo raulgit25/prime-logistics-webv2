c689c9c181a7de7d992b591fb05fe0edb4ca958e
unknown <raulhernandezgarcia2001@gmail.com>
2026-05-11 17:01:20 +0200

    primer código programado en modalInsertIncotermComp

--- resources/js/components/ModalInsertIncotermComponent.vue
+++ resources/js/components/ModalInsertIncotermComponent.vue
@@ -33,7 +33,7 @@
                             <input type="text" name="codi"
                                 class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50
                         border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autofocus />
+                                placeholder=" " autofocus v-model="incoterm.codi" />
                             <label class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10
                         origin-left start-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100
                         peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">
@@ -45,7 +45,7 @@
                             <textarea name="nom"
                                 class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50
                         border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" "></textarea>
+                                placeholder=" " v-model="incoterm.nom"></textarea>
                             <label class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10
                         origin-left start-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100
                         peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">
@@ -84,7 +84,13 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const incoterm = ref({});
+
+const insertIncoterms = () => {
+
+};
 </script>
 
 <style lang="scss" scoped></style>
