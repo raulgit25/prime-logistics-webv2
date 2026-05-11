3c494c89eed1ecabe56872df44295f65c6ab686d
unknown <raulhernandezgarcia2001@gmail.com>
2026-05-11 16:54:32 +0200

    modal insert incoterm añadido en app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import IncotermsComponent from './components/IncotermsComponent.vue';
 import TiposIncotermComponent from './components/tiposIncoterm/TiposIncotermComponent.vue';
 import TipoIncotermComponent from './components/tiposIncoterm/TipoIncotermComponent.vue';
+import ModalInsertIncotermComponent from './components/ModalInsertIncotermComponent.vue';
 
 const app = createApp({});
 app.component('incoterms-component', IncotermsComponent);
 app.component('tipos-incoterm-component', TiposIncotermComponent);
 app.component('tipo-incoterm-component', TipoIncotermComponent);
+app.component('modal-insert-incoterm-component', ModalInsertIncotermComponent);
 app.mount('#app');
