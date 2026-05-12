f0a9c85f8c0f93ff910ad42044fa9908dc1066b8
unknown <raulhernandezgarcia2001@gmail.com>
2026-05-12 17:17:13 +0200

    modal

--- resources/js/components/tiposIncoterm/TipoIncotermComponent.vue
+++ resources/js/components/tiposIncoterm/TipoIncotermComponent.vue
@@ -8,7 +8,7 @@
                 <button data-modal-target="update-modal" data-modal-toggle="update-modal" type="button"
                     class="p-2 rounded-lg bg-yellow-500 hover:bg-yellow-600 text-white transition">
                     <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" @click="emit('edit', incoterm)">
+                        stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
                     </svg>
