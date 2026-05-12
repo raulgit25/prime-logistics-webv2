6b20dbdb260be1465440b672d560e3e0cf89b9f8
raulh <raulhernandezg2001@gmail.com>
2026-05-12 23:47:41 +0200

    a

--- resources/js/components/IncotermsComponent.vue
+++ resources/js/components/IncotermsComponent.vue
@@ -26,48 +26,8 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import TiposIncotermComponent from './tiposIncoterm/TiposIncotermComponent.vue';
-import axios from 'axios';
-import { Modal } from 'flowbite';
 
 const componenteActivo = ref('tipos_incoterm');
-
-const estaCargando = ref(false);
-const esError = ref(false);
-const incoterm = ref({});
-const form = ref({
-    codi: '',
-    nom: ''
-});
-const mensajeError = ref();
-
-/*Coge los datos que hay en la BD y que puede modificar el usuario*/
-const cargarDatosBD = async () => {
-    const respuesta = await fetch('/api/tipus_incoterm');
-    form.value = await respuesta.json();
-};
-
-estaCargando.value = true;
-const updateIncoterms = (incoterm) => {
-    const targetEl = document.getElementById('update-modal');
-    const modal = new Modal(targetEl);
-
-    axios.put('tipus_incoterm', form.value)
-        .then((response) => {
-            estaCargando.value = true;
-            form.value = response.data;
-            emit('updateIncoterm');
-            modal.hide();
-        })
-        .catch((error) => {
-            estaCargando.value = false;
-            mensajeError.value = error.response.data.error;
-        });
-};
-
-onMounted(() => {
-    cargarDatosBD();
-    updateIncoterms();
-});
 </script>
